c39aab1af2ac1911e17db7a2ee75cd321e589c9f
luo1g <412696268@qq.com>
2020-11-17 14:04:43 +0800

    Initial commit

---
+++ src/com/luol/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.luol.java;
+
+import java.util.ArrayList;
+
+/**
+ * @author eg@dmin
+ * @createTime 2020-11-15 22:23
+ */
+public class HelloWorld {
+
+    public static void  main(String[] args){
+        System.out.println("hellowWorld---32432");
+        ArrayList list = new ArrayList<String>();
+    }
+}
